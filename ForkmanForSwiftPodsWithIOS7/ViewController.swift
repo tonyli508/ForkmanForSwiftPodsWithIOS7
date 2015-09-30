27b6f718fce0f70be8369eea13bebaef14e2e546
Li Jiantang <tonyli508@gmail.com>
2015-09-30 19:43:03 +0100

    Reame update, add small exmaple code

--- ForkmanForSwiftPodsWithIOS7/ViewController.swift
+++ ForkmanForSwiftPodsWithIOS7/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var textView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        request(.GET, "https://github.com/tonyli508/ForkmanForSwiftPodsWithIOS7/blob/master/README.md").responseString(encoding: NSUTF8StringEncoding) { [unowned self] (request, response, resultText, error) -> Void in
+            
+            if error != nil {
+                self.textView.text = error?.localizedDescription
+            } else {
+                self.textView.text = resultText
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Forkman For Swift Pods With IOS7
+##Forkman For Swift Pods With IOS7
+
+
+Reduce pain using Swift to support IOS7, full tutorial
+blog at https://medium.com/@tonyli508/forkman-for-swift-pods-with-ios-7-e467b9f8c0d5
+
+
+####Make sure Installing Xcodeproj:
+
+	$ [sudo] gem install xcodeproj
+
+
+
